62864190c854107e41a4a9cda235acfd678b23a1
Jeffrey Pallarés Núñez <jeff.uca@gmail.com>
2020-01-24 17:36:56 +0100

    [ADD] documentation of forwarding function

--- forwarding.m
+++ forwarding.m
@@ -1,14 +1,13 @@
 function [out] = forwarding(neural_net,out)
-%FORWARDING Summary of this function goes here
-%   Detailed explanation goes here
+%FORWARDING First phase of the backpropagation algorithm.
+%  With this function we will obtain the activation function ( the output) and the derivates of all the layers
+% and store them in the matrix out. This values are necessary to the backpropagation phase.
 
     for i=1:length(neural_net)
 
         z = out{end,2} * neural_net(i).W + neural_net(i).bias;
         [a,~] = neural_net(i).activation_function(z);
         out(i+1,1:2)={z,a};
-
-
     end
 
 end
