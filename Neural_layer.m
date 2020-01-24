023ee7b03748a62827c15b58d7a4d840c0bf5f70
Jeffrey Pallarés Núñez <jeff.uca@gmail.com>
2020-01-24 17:30:28 +0100

    [ADD]  documentation

--- Neural_layer.m
+++ Neural_layer.m
@@ -1,6 +1,6 @@
 classdef Neural_layer
-    %NEURAL_LAYER Summary of this class goes here
-    %   Detailed explanation goes here
+    %NEURAL_LAYER Class that represent a layer of a neural network.
+    %   attr: n_input_connections, activation_function, n_neurons, bias, w (weights).
     
     properties
         n_input_connections
@@ -13,19 +13,13 @@ classdef Neural_layer
     methods
         function obj = Neural_layer(n_input_connections,n_neurons,activation_function)
             %NEURAL_LAYER Construct an instance of this class
-            %   Detailed explanation goes here
+            %  the weights are initialized randomly with values between -1 and 1.
             
             obj.n_input_connections = n_input_connections;
             obj.n_neurons = n_neurons;
             obj.activation_function = activation_function;
-            obj.bias = ones(1,n_neurons) ; % entre menos -1 y 1
+            obj.bias = ones(1,n_neurons) ;
             obj.W = rand(n_input_connections, n_neurons) *2 -1; % entre -1 y 1
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
         end
         
         function obj = reset_bias(obj)
