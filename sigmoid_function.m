1da7d934ad43ece7fb95fa80ef122fc2094dedfd
Jeffrey Pallarés Núñez <jeff.uca@gmail.com>
2020-01-25 14:37:36 +0100

    [ADD] function Documentation

--- sigmoid_function.m
+++ sigmoid_function.m
@@ -1,7 +1,10 @@
 function [sigm,d_sigm] = sigmoid_function(x)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%SIGMOID_FUNCTION Function to calculate de sigmoid function and his derivate.
+
 sigm = 1./(1+exp(-x));
+
+% the derivate expression is expresed in this way to improve the 
+% eficiency. Look the way in which is used in the code and you will understand.
 d_sigm = x.*(1-x);
 end
 
