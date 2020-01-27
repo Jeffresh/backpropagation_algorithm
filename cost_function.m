1dade33a1b74543fc5179c7b5be4af4a0e892cbb
Jeffrey Pallarés Núñez <jeff.uca@gmail.com>
2020-01-27 18:58:48 +0100

    [ADD] Documentation

--- cost_function.m
+++ cost_function.m
@@ -1,6 +1,5 @@
 function [c,d_c] = cost_function(so,sd)
-%COST_FUNCTION Summary of this function goes here
-%   Detailed explanation goes 
+%COST_FUNCTION Mean scuared error and his derivated.
 
 c =mean((so-sd).^2);
 d_c = so-sd;
